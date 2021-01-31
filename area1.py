c6fd43da1abf33afc7420e4790fd4dadb2c756c9
Sowmiyaa123 <70963714+Sowmiyaa123@users.noreply.github.com>
2021-01-31 11:42:25 +0530

    Update area1.py

--- area1.py
+++ area1.py
@@ -2,10 +2,9 @@ def findArea(r):
     PI = 3.142
     return PI * (r*r); 
   
-# Driver method 
 print("Area is %.6f" % findArea(5)); 
   
-  
+  # task2
   filename = input("Input the Filename: ")
 f_extns = filename.split(".")
 print ("The extension of the file is : " + repr(f_extns[-1]))
